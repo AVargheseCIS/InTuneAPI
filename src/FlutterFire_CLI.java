3ef642dbd9b2ece770afd94dbb8a1e04fedf20d1
jason-zhang-github <89471571+jason-zhang-github@users.noreply.github.com>
2022-10-02 19:30:14 -0400

    Added Server Classes.

---
+++ src/FlutterFire_CLI.java
@@ -0,0 +1,15 @@
+public class FlutterFire_CLI {
+
+
+    /**
+     * Initializes flutterfire connection system.
+     */
+    private void flutterfire_configure()
+    {}
+
+
+
+    public FlutterFire_CLI()
+    {}
+
+}
